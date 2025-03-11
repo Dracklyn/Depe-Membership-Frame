d65b36652b7672ef6ddb1f397373f62fb9f3b2f4
Dracklyn <83720335+Dracklyn@users.noreply.github.com>
2025-03-11 21:48:42 +0100

    Update

---
+++ app/layout.tsx
@@ -0,0 +1,33 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import { config } from "@/config";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+	title: config.title,
+	description: "A channel frame",
+	other: {
+		"fc:frame": "vNext",
+		"fc:frame:image": config.startImage,
+		"fc:frame:button:1": "Request to Join",
+		"fc:frame:button:1:action": "post",
+		"fc:frame:button:1:target": " https://ad64-91-65-203-172.ngrok-free.app/api/join",
+		"fc:frame:button:2": "Source Code",
+		"fc:frame:button:2:action": "link",
+		"fc:frame:button:2:target": "https://github.com/PinataCloud/channel-frame",
+	},
+};
+
+export default function RootLayout({
+	children,
+}: Readonly<{
+	children: React.ReactNode;
+}>) {
+	return (
+		<html lang="en">
+			<body className={inter.className}>{children}</body>
+		</html>
+	);
+}
---
+++ config.ts
@@ -0,0 +1,16 @@
+export const config = {
+	title: "Join /depe",
+	channel: "depe",
+	startImage:
+		"https://res.cloudinary.com/verifiedcreators/image/upload/v1739232925/DEPE/DEPE-Banner-Bg_bk79ec.png",
+	mustFollowImage:
+		"https://res.cloudinary.com/verifiedcreators/image/upload/v1741078437/DEPE/Frame_918_y3gd14.png",
+	alreadyMemberImage:
+		"https://res.cloudinary.com/verifiedcreators/image/upload/v1741078815/DEPE/Frame_922_ybxext.png",
+	successImage:
+		"https://res.cloudinary.com/verifiedcreators/image/upload/v1741078437/DEPE/Frame_919_kz6aj1.png",
+	errorImage:
+		"https://res.cloudinary.com/verifiedcreators/image/upload/v1741078437/DEPE/Frame_920_e4lce6.png",
+	insufficientBalanceImage:
+		"https://res.cloudinary.com/verifiedcreators/image/upload/v1741078437/DEPE/Frame_921_du7jky.png",
+};
